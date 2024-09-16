cf27107ba5c0919f344636117b5f78586ad60b62
praneeth622 <praneethdevarasetty31@gmail.com>
2024-09-16 21:35:35 +0530

    init 2

--- app/(Pages)/Home/page.tsx
+++ app/(Pages)/Home/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>This is Home Page</div>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import { redirect } from 'next/navigation';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+export default function HomeRedirect() {
+  redirect('/Home'); // Redirects to the Home page
 }
-
-export default page
